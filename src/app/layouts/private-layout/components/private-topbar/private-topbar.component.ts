e7376837fac477aed01937f083a868d1c108d9f9
Evy38 <cecilemorel0907@gmail.com>
2026-01-23 11:31:54 +0100

    topbar ok

--- src/app/layouts/private-layout/components/private-topbar/private-topbar.component.ts
+++ src/app/layouts/private-layout/components/private-topbar/private-topbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { filter, map, startWith } from 'rxjs';
 
 type TopbarContext = 'dashboard' | 'annonces' | 'candidatures' | 'relances' | 'profil';
 
@@ -19,6 +19,7 @@ export class PrivateTopbarComponent {
   // Observable du contexte topbar en fonction de la route active
   topbarContext$ = this.router.events.pipe(
     filter((e) => e instanceof NavigationEnd),
+    startWith(null),
     map(() => this.getDeepestChild(this.route)),
     map((r) => (r.snapshot.data['topbar'] as TopbarContext) ?? 'dashboard')
   );
