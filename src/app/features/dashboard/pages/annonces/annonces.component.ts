27d24e702ee26f85a5c4164fef5f0db3b6329071
Evy38 <cecilemorel0907@gmail.com>
2026-01-26 11:09:22 +0100

    liste des annonces ok

--- src/app/features/dashboard/pages/annonces/annonces.component.ts
+++ src/app/features/dashboard/pages/annonces/annonces.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { JobService } from '../../services/job.service';
+import { Job } from '../../models/job.model';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-annonces',
   standalone: true,
-  template: `<h1>Annonces</h1><p>Page placeholder.</p>`,
+  imports: [CommonModule],
+  templateUrl: './annonces.component.html',
+  styleUrls: ['./annonces.component.css'],
 })
-export class AnnoncesComponent {}
+export class AnnoncesComponent implements OnInit {
+  jobs: Job[] = [];
+  loading = true;
+  error: string | null = null;
+
+  constructor(private jobService: JobService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.jobService.getJobs().subscribe({
+      next: (jobs) => {
+        console.log('Réponse API jobs:', jobs);
+        this.jobs = jobs;
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Erreur API jobs:', err);
+        this.error = 'Impossible de charger les annonces';
+        this.loading = false;
+      },
+    });
+  }
+}
