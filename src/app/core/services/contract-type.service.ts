0e8cae70a7710c6cf0b89a56cdfaf2813782d49a
Evy38 <cecilemorel0907@gmail.com>
2026-02-05 09:16:19 +0100

    filtre tupe contrats fonctionnels

--- src/app/core/services/contract-type.service.ts
+++ src/app/core/services/contract-type.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 export interface ContractType {
   value: string;
   label: string;
 }
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class ContractTypeService {
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/jobs/contract-types`; 
 
   getContractTypes(): Observable<ContractType[]> {
-    return this.http.get<ContractType[]>('/api/contract-types');
+    return this.http.get<ContractType[]>(this.apiUrl);
   }
 }
