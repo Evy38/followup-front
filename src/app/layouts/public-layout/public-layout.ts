87d5488c2510f388c0cd3dce0f7e01cfa17ab08a
Evy38 <cecilemorel0907@gmail.com>
2026-02-09 10:45:47 +0100

    ajout component footer + page A propos

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../shared/public-navbar/public-navbar.component';
 import { ToastComponent } from '../../shared/toast/toast.component';
+import { PublicFooterComponent } from '../../shared/public-footer/public-footer.component';
 
 @Component({
   selector: 'app-public-layout',
@@ -13,6 +14,7 @@ import { ToastComponent } from '../../shared/toast/toast.component';
     NavbarComponent,
     ToastComponent,
     RouterOutlet,
+    PublicFooterComponent,
   ],
 })
 export class PublicLayoutComponent {}
---
+++ src/app/shared/public-footer/public-footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-footer',
+  standalone: true,
+  templateUrl: './public-footer.component.html',
+  styleUrls: ['./public-footer.component.css'],
+})
+export class PublicFooterComponent {}
