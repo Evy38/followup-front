afcd84b79addbd24d5667115a00ddaa135a6b084
Evy38 <cecilemorel0907@gmail.com>
2026-02-13 13:18:16 +0100

    toast vue profil ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './shared/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastComponent],
   template: `
     <router-outlet></router-outlet>
     <router-outlet name="overlay"></router-outlet>
+    <app-toast></app-toast>
   `,
 })
 export class AppComponent {}
